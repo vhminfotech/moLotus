db426910ae2e63b99b2cc34229e5d3d6c0cbf3d6
yashpwr <yashdvn@gmail.com>
2021-09-16 11:32:31 +0530

    Gradle build name, font removed, random color

--- presentation/src/main/java/com/sms/moLotus/common/util/FontProvider.kt
+++ presentation/src/main/java/com/sms/moLotus/common/util/FontProvider.kt
@@ -14,20 +14,20 @@ class FontProvider @Inject constructor(context: Context) {
     private var lato: Typeface? = null
     private val pendingCallbacks = ArrayList<(Typeface) -> Unit>()
 
-    init {
-        ResourcesCompat.getFont(context, R.font.lato, object : ResourcesCompat.FontCallback() {
-            override fun onFontRetrievalFailed(reason: Int) {
-                Timber.w("Font retrieval failed: $reason")
-            }
-
-            override fun onFontRetrieved(typeface: Typeface) {
-                lato = typeface
-
-                pendingCallbacks.forEach { lato?.run(it) }
-                pendingCallbacks.clear()
-            }
-        }, null)
-    }
+//    init {
+//        ResourcesCompat.getFont(context, R.font.lato, object : ResourcesCompat.FontCallback() {
+//            override fun onFontRetrievalFailed(reason: Int) {
+//                Timber.w("Font retrieval failed: $reason")
+//            }
+//
+//            override fun onFontRetrieved(typeface: Typeface) {
+//                lato = typeface
+//
+//                pendingCallbacks.forEach { lato?.run(it) }
+//                pendingCallbacks.clear()
+//            }
+//        }, null)
+//    }
 
     fun getLato(callback: (Typeface) -> Unit) {
         lato?.run(callback) ?: pendingCallbacks.add(callback)
