465b43b4ada7d865f851479e30d313169671ae0e
VHM Infotecch <89240446+vhminfotech@users.noreply.github.com>
2022-04-25 19:11:16 +0530

    Update Navigator.kt

--- presentation/src/main/java/com/sms/moLotus/common/Navigator.kt
+++ presentation/src/main/java/com/sms/moLotus/common/Navigator.kt
@@ -92,10 +92,10 @@ class Navigator @Inject constructor(
         }
     }
 
-    fun showCompose(sub: String? = null,body: String? = null, images: List<Uri>? = null) {
+    fun showCompose(sub: String? = null, body: String? = null, images: List<Uri>? = null) {
         val intent = Intent(context, ComposeActivity::class.java)
-        intent.putExtra(Intent.EXTRA_TEXT, body )
-        intent.putExtra(Intent.EXTRA_SUBJECT, sub)
+        intent.putExtra(Intent.EXTRA_TEXT, body)
+        intent.putExtra(Intent.EXTRA_SUBJECT, "<Subject: Fwd: $sub>")
         Timber.e("==========", "images:: $images")
         images?.takeIf { it.isNotEmpty() }?.let {
             intent.putParcelableArrayListExtra(Intent.EXTRA_STREAM, ArrayList(images))
