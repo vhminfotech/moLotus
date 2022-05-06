df9e9c19fea83704260507466f97c8ca675e061b
VHM Infotecch <89240446+vhminfotech@users.noreply.github.com>
2022-05-06 14:41:03 +0530

    Update Constants.kt

--- presentation/src/main/java/com/sms/moLotus/feature/Constants.kt
+++ presentation/src/main/java/com/sms/moLotus/feature/Constants.kt
@@ -6,4 +6,10 @@ object Constants {
     const val CARRIER_ID = 1
     const val CARRIER_NAME = "Telkomsel"
     const val FOLDER_NAME = "MGRAM/Media"
+    const val TOKEN = "token"
+    const val DRAFT_SAVED = "draft"
+    const val USERID = "userId"
+    const val DUMMY_NAME = "Akanksha Gogavale"
+    const val TAB_SMS = "MESSAGES"
+    const val TAB_CHAT = "CHATS"
 }
