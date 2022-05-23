387c7eda0d2520b8ae9b1f6fde29eb6951143e51
VHM Infotecch <89240446+vhminfotech@users.noreply.github.com>
2022-05-23 19:00:58 +0530

    changes in mutation

--- presentation/src/main/java/com/sms/moLotus/feature/Constants.kt
+++ presentation/src/main/java/com/sms/moLotus/feature/Constants.kt
@@ -3,7 +3,7 @@ package com.sms.moLotus.feature
 object Constants {
     //const val BASE_URL = "http://mgram-backend.vhminfotech.com/api/"
     const val BASE_URL = "http://dev-mgram.vhminfotech.com/api/"
-    const val BASE_GRAPHQL_URL = "https://203c-122-170-57-138.in.ngrok.io/graphql"
+    const val BASE_GRAPHQL_URL = "https://6c1a-122-170-57-138.in.ngrok.io/graphql"
     const val SOCKET_URL = "http://dev-mgram.vhminfotech.com/api/"
     const val CARRIER_ID = 1
     const val CARRIER_NAME = "Telkomsel"
